beab0425e24ed8dc074683a16435f339ca68e664
yuanma <yuanma@email.arizona.edu>
2014-11-12 14:45:33 -0700

    added javadoc and comments to the class

--- src/model/Building.java
+++ src/model/Building.java
@@ -4,27 +4,48 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.io.Serializable;
 
+/**
+ * This class contain the basic attributes for the buildings.
+ * 
+ * @author Yuanjun Ma
+ *
+ */
+
 public abstract class Building implements Serializable {
-	private int cost;
+	private int buildCost;
 	private Point location;
 	private Dimension dimension;
 	private String resource;
 	
-	public Building(int x, int y, int width, int height, String source) {
+	/**
+	 * Construct the Building object, the parameters present the location, size, and function of the building.
+	 * @param x an {@link Integer} represent the location of the building on x axis.
+	 * @param y an {@link Integer} represent the location of the building on y axis.
+	 * @param width an {@link Integer} represent the width of the building on the map.
+	 * @param length an {@link Integer} represent the length of the building on the map.
+	 * @param source a {@link String} represent the purpose of the building.
+	 */
+	public Building(int x, int y, int width, int length, String source) {
 		location = new Point(x, y);
-		dimension = new Dimension(width, height);
+		dimension = new Dimension(width, length);
 		resource = source;
 	}
 	
+	
 	public void setCost(int price) {
-		this.cost = price;
+		this.buildCost = price;
 	}
 	
+	// Return the location of the building on the map
 	public Point getLocation() {
 		return location;
 	}
-	
+	// Return the cost of resource to construct the building
 	public int getCost() {
-		return cost;
+		return buildCost;
+	}
+	// Return the size of the building
+	public Dimension getDimension() {
+		return dimension;
 	}
 }
