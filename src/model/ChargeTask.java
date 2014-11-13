eda9b0c728583ed1cab3b866afa1c1d3a1198ae4
Gabe <serranof1@email.arizona.edu>
2014-11-13 12:56:41 -0700

    Added new Tasks to list of tasks

---
+++ src/model/ChargeTask.java
@@ -0,0 +1,18 @@
+package model;
+
+public class ChargeTask extends Task{
+
+	public ChargeTask(Drone drone) {
+		super(drone);
+		
+	}
+
+	@Override
+	public void execute(Drone current) {
+	
+		
+	}
+	
+
+	
+}
---
+++ src/model/MoveTask.java
@@ -0,0 +1,15 @@
+package model;
+
+public class MoveTask extends Task {
+
+	public MoveTask(Drone drone) {
+		super(drone);
+
+	}
+
+	@Override
+	public void execute(Drone current) {
+
+	}
+
+}
