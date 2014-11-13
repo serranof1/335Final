088a529c5afc92c702a06937ae7a264bbd37ad5a
Gabe <serranof1@email.arizona.edu>
2014-11-13 15:16:08 -0700

    Updated Drone

--- src/model/Drone.java
+++ src/model/Drone.java
@@ -4,19 +4,8 @@ import java.util.LinkedList;
 
 public class Drone {
 	
-	/**
-	 * Private instance variable that will be use to determine if the drone is
-	 * currently doing anything. The default will be the doNothing command
-	 */
-	private Command currentJob;
-	
-	/**
-	 * Private linked list to keep track of the specific drones commands. As the
-	 * drone gets more and more things to do if it needs power it will start
-	 * moving further and further up the list until it is first then it will
-	 * stop what its doing and go get power.
-	 */
-	private LinkedList<Command> taskList;
+	private int locationX;
+	private int locationY;
 	
 	/**
 	 * Keeps track of the drone's current power level. Depletes over time by
@@ -27,24 +16,23 @@ public class Drone {
 	 * 
 	 */
 	private double power;
+
 	/**
-	 * Keeps track of whether the drone is charging
-	 */
-	private boolean charging;
-	
-	/**
-	 * Instance variable that holds location of drone on the map
-	 */
-	private int locationX;
-	private int locationY;
-	
-	/**
-	 * Instance variable for the capacity amount of weight a drone currently holds.
+	 * Instance variable for the amount of materials a drone currently holds.
 	 * Later we can implement a cap, and increase movement cost if the drone
 	 * 	has more things.
 	 */
-	private double inventory;
-
+	private double materials;
+	
+	/**
+	 * Instance variable for the current tile the drone is on.
+	 */
+	private Tile currentTile;
+	
+	/**
+	 * Instance keeps track of the drones charging state;
+	 */
+	private boolean charging;
 	
 	/**
 	 * Creates a drone with a set amount of starting power, and a default job of do nothing
@@ -53,12 +41,12 @@ public class Drone {
 	 * @param defaultJob 	The Default job that a drone will always do
 	 */
 
-	public Drone(double power, Command defaultJob) {
+	public Drone(double power, Tile start) {
 		this.power = power;
-		this.currentJob = defaultJob;
-		this.charging = false;
-		this.locationX = 0; 
-		this.locationY = 0;			//This should be set to wherever the drones are made
+		currentTile = start;
+		materials = 0;
+		
+		
 	}
 	
 	//Getters and setters for location
@@ -84,59 +72,6 @@ public class Drone {
 		else
 			return false;
 	}
-
-	public void doNothing() {
-		// TODO Auto-generated method stub
-	}
-
-//	private Comparator<Tile> tileSorter = new Comparator<Tile>() {
-//
-//		public int compare(Tile tile0, Tile tile1) {
-//			if (n1.fCost < n0.fCost)
-//				return 1;
-//			if (n1.fcost > n0.fCost)
-//				return -1;
-//			return 0;
-//		}
-//	};
-//	
-//	
-//  The beginnings of the A* Algorithm
-//	public List<Tile> findPath(Vector21 start, Vector21 goal){
-//		List<Tile> openList = new ArrayList<Tile>();
-//		List<Tile> closedList = new ArrayList<Tile>();
-//		Tile current = new Tile(start, null, 0, getDistance(start, goal));
-//		openList.add(current);
-//		while(openList.size() > 0){
-//			Collections.sort(openList, tileSorter);
-//			current = openList.get(0);
-//			if(current.equals(goal)){
-//				//return...
-//			}
-//			openList.remove(current);
-//			closedList.add(current);
-//			
-//			for(int i = 0; i<9; i++){
-//				if(i == 4) continue;
-//				int x = current.getx();
-//				int y = current.getY();
-//				int xi = (i % 3) - 1;
-//				int yi = (i / 3) - 1;
-//				Tile at = getTile(x + xi, y+yi);
-//				if(at = null) continue;
-//				if(at.solid()) continue;
-//				Vector2i a = new Vector2i(x+xi, y+yi);
-//				double gCost = current.gCost + getDistance(current, a);
-//				
-//			}
-//		}
-//	}
-//
-//	private double getDistance(Vector2i tile, Vector21 goal) {
-//		double dx = tile.getX() - goal.getX();
-//		double dy = tile.getY() - goal.getY();
-//		return Math.sqrt(dx * dx + dy * dy);
-//	}
 
 	/**
 	 * This code will be executed depending on when the drones' power updates.
