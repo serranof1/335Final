185b9f5b8d47e359d42cfeaa9c6eb28723df1703
Gabe <serranof1@email.arizona.edu>
2014-11-14 15:09:49 -0700

    Fixed ChargeTask contructor

--- src/model/ChargeTask.java
+++ src/model/ChargeTask.java
@@ -2,8 +2,8 @@ package model;
 
 public class ChargeTask extends Task{
 
-	public ChargeTask(Drone drone) {
-		super(drone);
+	public ChargeTask(Drone drone, LinkedList<Tile> steps, Tile tile ) {
+		super(drone, steps, tile);
 		
 	}
 
