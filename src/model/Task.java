c05b20744bacf32d209693f025144b031639d90b
Gabe <serranof1@email.arizona.edu>
2014-11-14 09:37:21 -0700

    Added private instance variables to Task

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,7 +1,18 @@
 package model;
 
-public interface Task {
+import java.util.List;
+
+
+public abstract class Task {
+	private Drone drone;
+	private List<Tile> steps;
+	private Tile tile; 
 	
 	public abstract void execute(Drone current);
-
+	
+	public Task(Drone drone, List<Tile> steps, Tile tile){
+		this.drone = drone;
+		this.steps= steps;
+		this.tile = tile;
+	}
 }
