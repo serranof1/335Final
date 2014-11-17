382603a885cba7a8103c2dbfe36ca252184bcf0f
yuanma <yuanma@email.arizona.edu>
2014-11-17 14:07:30 -0700

    added the MineBuilding, and SolarPlant class, slightly changed methods in Building class

---
+++ src/model/MineBuilding.java
@@ -0,0 +1,12 @@
+package model;
+
+import resources.Resource;
+
+public class MineBuilding extends Building{
+
+	public MineBuilding(int x, int y, int width, int length, Resource source) {
+		super(x, y, width, length, source);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/model/SolarPlant.java
@@ -0,0 +1,12 @@
+package model;
+
+import resources.Resource;
+
+public class SolarPlant extends Building {
+
+	public SolarPlant(int x, int y, int width, int length, Resource source) {
+		super(x, y, width, length, source);
+		// TODO Auto-generated constructor stub
+	}
+
+}
