cce6c6fe449b399f512abaadfccc82c2ce53ea09
csjensen1 <csjensen@email.arizona.edu>
2014-12-07 14:36:34 -0700

    able to select tiles on main gui now

--- src/view/GraphicView.java
+++ src/view/GraphicView.java
@@ -6,6 +6,9 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JPanel;
 
@@ -14,8 +17,14 @@ public class GraphicView extends JPanel{
 	private Map map;
 	private int leftRow, leftCol;
 	private int viewLength, viewHeight;
-	private TextView textView;
+
+	private int startPointX, startPointY, endPointY, endPointX;
+	private int clicks = 0;
 	
+	private TextView textView;
+	private boolean selectResource;
+	public boolean dragging;
+
 	public GraphicView(Map map, int row, int col , int viewHeight, int viewLength, TextView textView) {
 
 		this.setSize(1000,1000);
@@ -25,32 +34,33 @@ public class GraphicView extends JPanel{
 		this.textView = textView;
 		leftRow = row;
 		leftCol = col;
-		
+		selectResource = true;
 		this.map = map;
 		this.addKeyListener(new KeyMoveListener());
-		
+		registerListeners();
+
 	}
 
 	public void setLeftCol(int input){
 		leftCol += input;
 	}
-	
+
 	public void setLeftRow(int input){
 		leftRow += input;
 	}
-	
+
 	public int getLeftCol(){
 		return leftCol;
 	}
-	
+
 	public int getLeftRow(){
 		return leftRow;
 	}
 
 
-	
+
 	private void drawMap() {
-	
+
 		repaint();
 	}
 
@@ -60,16 +70,28 @@ public class GraphicView extends JPanel{
 
 		int row = leftRow;
 		int col = leftCol;
-		
+
 		for (int i = 0; i < viewHeight; i++){
 			for (int j = viewLength; j > 0; j--) { // Changed loop condition here.
-				
+
 				map.getTile(col, row).draw(g, (viewLength - j) * 50, i * 50);
 				col++;
 			}
 			col = leftCol;
 			row++;
 		}
+
+		if(selectResource && clicks == 1){
+			int topLeftX = startPointX / 50;
+			int topLeftY = startPointY / 50;
+			int bottomRightX = endPointX / 50;
+			int bottomLeftY = endPointY / 50;
+			Color myGreen = new Color(0 , 255 , 0, 125);
+			System.out.println("drawing a rectangle:  "+topLeftX +"  " +topLeftY);
+			g.setColor(myGreen);
+			g.fillRect(topLeftX * 50, topLeftY * 50, (bottomRightX - topLeftX) * 50, (bottomLeftY - topLeftY) * 50);
+		}
+
 	}
 
 	public int getViewLength() {
@@ -88,6 +110,66 @@ public class GraphicView extends JPanel{
 		this.viewHeight = viewHeight;
 	}
 
+
+
+	private void registerListeners() {
+
+		MouseListener listener = new ListenToMouse();
+		MouseMotionListener motionListener = new ListenToMouse();
+
+		this.addMouseMotionListener(motionListener);
+		this.addMouseListener(listener);		
+	}
+	/**
+	 * This is where we tell the mouse what to do on clicks and movement
+	 * @author Cody Jensen
+	 *
+	 */
+	private class ListenToMouse implements MouseMotionListener, MouseListener {
+
+		
+		
+		public void mouseClicked(MouseEvent evt) {
+			
+			if(selectResource && clicks == 0){
+				startPointX = evt.getX();
+				startPointY = evt.getY();
+				clicks++;
+				System.out.println("mouse click 1:  " +clicks);
+				
+			} else if(selectResource && clicks == 1){
+				System.out.println("mouse click 2:   " +clicks);
+				clicks--;
+			} else {
+				//do other thigns here, im not sure what
+			}
+		}
+
+		public void mouseMoved(MouseEvent evt) {
+			endPointX = evt.getX();
+			endPointY = evt.getY();
+			if(selectResource && clicks == 1)
+				System.out.println("moving");
+				repaint();
+		}
+
+		public void mousePressed(MouseEvent evt) {
+		}
+
+		public void mouseEntered(MouseEvent evt) {
+		}
+
+		public void mouseReleased(MouseEvent evt) {
+		}
+
+		public void mouseExited(MouseEvent evt) {
+		}
+
+		public void mouseDragged(MouseEvent evt) {
+		
+		}
+	}
+
 	private class KeyMoveListener implements KeyListener
 	{
 		public void keyPressed(KeyEvent arg0) {
@@ -97,7 +179,7 @@ public class GraphicView extends JPanel{
 				if(leftRow > 0){
 					leftRow--;
 					textView.setLeftRow(-1);
-					
+
 					textView.repaint();
 					repaint();
 				} 
@@ -139,8 +221,12 @@ public class GraphicView extends JPanel{
 		public void keyTyped(KeyEvent arg0) {}
 	}
 
+	public void setSelectResource(){
+		
+	}
+	
 	public void setMap(Map map) {
 		this.map = map;
-		
+
 	}
 }
